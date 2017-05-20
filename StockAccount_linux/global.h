d1637ac129ba55c90ed8c36f75b5394d223512b3
skypanda100 <236520700@qq.com>
2017-05-20 18:13:01 +0800

    1.add stockaccount

---
+++ StockAccount_linux/global.h
@@ -0,0 +1,34 @@
+//
+// Created by zhengdongtian on 17-5-20.
+//
+
+#ifndef STOCKACCOUNT_GLOBAL_H
+#define STOCKACCOUNT_GLOBAL_H
+
+#include <stdlib.h>
+
+/**
+ * html
+ */
+struct Chunk
+{
+    char *memory;
+    size_t size;
+};
+
+/**
+ * db info
+ */
+static const char *DB_HOST = "127.0.0.1";
+//static const char *DB_HOST = "192.168.1.3";
+static const char *DB_PORT = "15432";
+static const char *DB_NAME = "postgres";
+static const char *DB_UID = "postgres";
+static const char *DB_PWD = "123456";
+
+/**
+ * target url
+ */
+static const char *TARGET_URL = "http://www.chinaclear.cn/cms-search/view.action?action=china&viewType=&dateStr=&channelIdStr=";
+
+#endif //STOCKACCOUNT_GLOBAL_H
